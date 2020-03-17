3384a28ac9cb00f4d01b0fc23998403d9b4a184d
Roman Kurbanmagomedov <kurbanmagomedovr@gmail.com>
2020-03-17 19:15:36 +0400

    version 2.0

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from difflib import get_close_matches as gcm
 data = json.load(open("data.json"))
 
 def get_value_by_key(key):
-    while True:
-        if key in data.keys():
-            return data[key]
-            break
+    if key in data.keys():
+        return str(data[key])
+            
+    else:
+        analog_key = gcm(key, data, n=1)
+        yn = input("the word is not in dictionary. did u mean %s? Enter Y or N: " % analog_key).lower()
+
+        if len(analog_key) > 0 and yn == 'y':
+            return data[analog_key[0]]
+                
+        elif len(analog_key) > 0 and yn == 'n':
+            return "Sorry(((. Try another word."
+            
         else:
-            analog_key = gcm(key, data, n=1)
-            if input("the word is not in dictionary. did u mean %s? Enter Y or N: " % analog_key).lower() == 'y':
-                return data[analog_key[0]]
-                break
-            else:
-                return "Sorry(((. Try another word."
-                break
+            return "we didn`t understent your input."
             
     
 word = input("enter the word: ")
